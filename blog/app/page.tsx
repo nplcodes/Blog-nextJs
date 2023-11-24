60fc44f3b502e03cc1ff8349a50de08b3566ba9c
NPL Codes <145145606+nplcodes@users.noreply.github.com>
2023-11-24 22:30:06 +0200

    blogs ..

---
+++ blog/app/page.tsx
@@ -0,0 +1,10 @@
+import FOrmNewPost from "@/components/form-new-post";
+
+export default function Home() {
+  return (
+    <main className="max-w-4xl mx-auto my-5">
+      <FOrmNewPost />
+    </main>
+  )
+}
+
